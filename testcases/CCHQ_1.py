f4eaafa47146f26fe20dcd5fae0bfc8be2e7f327
crimsonsun25 <65016488+crimsonsun25@users.noreply.github.com>
2020-08-12 18:54:17 +0530

    Update CCHQ_1.py

--- testcases/CCHQ_1.py
+++ testcases/CCHQ_1.py
@@ -1,35 +1,46 @@
 # Import the libraries and packages we will need and create a Unit Test Class that will have
 # a setup, a teardown and our first test method (or test case).
 import unittest
-from pageobjects.homescreen import HomeScreen
+from pageobjects.exports import Exports
 from pageobjects.login import Login
 from webdriver import Driver
-from values import strings
+from values import inputs
+import time
+
 
 class TestCCHQ(unittest.TestCase):
 
     def setUp(self):
         self.driver = Driver()
-        #self.driver.maximize_window()
-        self.driver.navigate(strings.base_url)
-        login_page= Login(self.driver)
-        login_page.enter_username(strings.login_username)
-        login_page.enter_password(strings.login_password)
-        login_page.click_submit()
+        self.driver.navigate(inputs.base_url)
+        login_page = Login(self.driver)
         login_page.accept_cookies()
+        login_page.enter_username(inputs.login_username)
+        login_page.enter_password(inputs.login_password)
+        login_page.click_submit()
 
-    print("Done")
 
-    #def test_home_screen_components(self):
-    #   home_screen = Homescreen(self.driver)
-    #   home_screen.validate_title_is_present()
-    #   home_screen.validate_icon_is_present()
-    #   home_screen.validate_menu_options_are_present()
-    #   home_screen.validate_posts_are_visible()
-    #   home_screen.validate_social_media_links()
+    def test_exports(self):
+        driver = self.driver
+        export = Exports(driver)
+        try:
+            export.form_exports()
+            time.sleep(2)
+            export.validate_downloaded_form_exports()
+            time.sleep(2)
+            export.case_exports()
+            time.sleep(2)
+            #export.validate_downloaded_case_exports()
 
-    def tearDown(self):
-        self.driver.instance.quit()
+        #except Exception as e:
+         #   print(e)
+        finally:
+            print("We are in!")
+            time.sleep(2)
+
+
+    # def tearDown(self):
+    # self.driver.instance.quit()
 
 
 if __name__ == '__main__':
